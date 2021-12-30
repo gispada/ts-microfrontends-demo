19c09636d7bfcdf45c59965c8314209bbff80b30
Gianluca Spada <gianluca.spada@spindox.it>
2021-12-30 12:26:51 +0100

    Fix package script

--- packages/container/src/pages/Home.tsx
+++ packages/container/src/pages/Home.tsx
@@ -14,16 +14,19 @@ const Home = () => {
       <Text h1>TypeScript Microfrontends</Text>
       <Text>There are five projects making up this application.</Text>
       <Text h5>Container</Text>
-      <Text>The shell with the header and the side panel. Handles routing.</Text>
+      <Text>
+        The shell with the header and the side panel. Handles the top level routing.
+      </Text>
       <Text h5>Account</Text>
       <Text>React application showing a basic account section. Has two subroutes.</Text>
       <Text h5>Dashboard</Text>
       <Text>
-        React application with random data displayed in charts and a table. Has two subroutes.
+        React application with random data displayed in charts and a table. Has three
+        subroutes, one of which consumes another frontend.
       </Text>
       <Text h5>Product</Text>
       <Text>
-        Vue3 application showing details on the items listed in the Dashboard table.
+        Vue 3 application showing details on the items listed in the Dashboard table.
       </Text>
       <Text h5>Shared</Text>
       <Text>
