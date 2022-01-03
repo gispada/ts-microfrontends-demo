92db645442c262a2660d408ce803a0a1b3019292
Gianluca Spada <gianluca.spada@spindox.it>
2022-01-03 16:09:43 +0100

    Minor style fix - 2

--- packages/shared/src/components/Card/styled.ts
+++ packages/shared/src/components/Card/styled.ts
@@ -5,4 +5,10 @@ export const StyledCard = styled(Card)<{ $height?: number }>`
   box-shadow: 0px 8px 24px rgba(149, 157, 165, 0.1);
   border-radius: 8px;
   height: ${({ $height }) => ($height ? `${$height}px` : '100%')};
+
+  @media (max-width: 479px) {
+    .ant-card-body {
+      padding: 24px 12px;
+    }
+  }
 `
